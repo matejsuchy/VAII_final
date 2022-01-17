24da6f5a15ba4d64d4c450b762b92d969e3b7505
Matej Suchý <matej.suchy@studentstc.sk>
2022-01-18 00:38:22 +0200

    drobny fix v js

--- src/public/js/hra.js
+++ src/public/js/hra.js
@@ -5,94 +5,83 @@ let casovacElement = document.querySelector(".timer");
 const DEFAULT_TIME = 60;
 let casovac = null;
 
+const RANDOM_QUOTE_API_URL = "http://api.quotable.io/random";
 
-const hlaskyPolitikov = [
-    ["Žiaden človek nedokáže dať toľko lásky človeku, ako dokáže len človek človeku.", "Kpt.JUDr.Danko"],
-    ["Dočkaj času jak hus klasu. Prepáčte, že tykám.", "Mgr.Igor Matovič"],
-    ["Môžem vám aj za-re-po-vať.", "JUDr. Štefan Harabin"],
-    ["Preto som sa rozhodol tu, z tejto maštale povedať pravdu slovenskej verejnosti!", "Ing. Miroslav Jureňa"],
-    ["Z dlhodobého hľadiska sme všetci mŕtvi.", "Ing. Richard Sulík"],
-    ["My pôjdeme do tankoch a pôjdeme a zrovnáme Budapešť!", "Ing. Ján Slota"],
-    ["Prosím vás neotravujte tu sme pri žatve.", "Doc JUDr Robert Fico, CSc"],
-    ["Tá choroba existuje, ale je vymyslená a je to ďalší druh chrípky.", "Ing. Mgr Marian Kotleba"]
-];
-
-let aktualnaHlaska = dajHlasku();
+let aktualnaHlaska;
 let aktualnyText = "";
 
 let uplynulyCas = 0;
 let zostavajuciCas = 60;
 
-function inizializuj() {
-    rewriteText.innerText = aktualnaHlaska[0];
-    autor.innerText = aktualnaHlaska[1];
+async function inizializuj() {
+  aktualnaHlaska = await dajHlasku();
+  rewriteText.innerText = aktualnaHlaska.content;
+  autor.innerText = aktualnaHlaska.author;
 
-    textArea.addEventListener('click', spustiHru);
-    textArea.addEventListener('input', spracujAktualnyText);
+  textArea.addEventListener("click", spustiHru);
+  textArea.addEventListener("input", spracujAktualnyText);
 }
 
 function dajHlasku() {
-    let dlzka = hlaskyPolitikov.length;
-    let rndIndex = Math.floor(Math.random() * dlzka);
-    return hlaskyPolitikov[rndIndex];
+  return fetch(RANDOM_QUOTE_API_URL)
+    .then((response) => response.json())
+    .then((data) => data);
 }
 
 function spracujAktualnyText() {
-    aktualnyText = textArea.value;
-    if (aktualnyText.length < aktualnaHlaska[0].length) {
-        aktualnyTextZnaky = aktualnyText.split('');
-        spansAll = document.querySelectorAll('.span_char');
-        spansAll.forEach((aktualnySpan, index) => {
-            aktualnyZnak = aktualnyTextZnaky[index];
+  let finsihed = true;
+  aktualnyText = textArea.value;
+  if (aktualnyText.length < aktualnaHlaska.content.length) {
+    aktualnyTextZnaky = aktualnyText.split("");
+    spansAll = document.querySelectorAll(".span_char");
+    spansAll.forEach((aktualnySpan, index) => {
+      aktualnyZnak = aktualnyTextZnaky[index];
 
-            if (aktualnyZnak == null) {
-                aktualnySpan.classList.remove('matchOK');
-                aktualnySpan.classList.remove('matchNOK');
-            } else if (aktualnyZnak === aktualnySpan.innerText) {
-                aktualnySpan.classList.add('matchOK');
-                aktualnySpan.classList.remove('matchNOK');
-            } else {
-                aktualnySpan.classList.add('matchNOK');
-                aktualnySpan.classList.remove('matchOK');
-            }
-        });
-    }
+      if (aktualnyZnak == null) {
+        aktualnySpan.classList.remove("matchOK");
+        aktualnySpan.classList.remove("matchNOK");
+        finsihed = false;
+      } else if (aktualnyZnak === aktualnySpan.innerText) {
+        aktualnySpan.classList.add("matchOK");
+        aktualnySpan.classList.remove("matchNOK");
+      } else {
+        aktualnySpan.classList.add("matchNOK");
+        aktualnySpan.classList.remove("matchOK");
+        finsihed = false;
+      }
+    });
+  }
 }
 
 function spustiHru() {
+  oznacZnaky();
 
-    oznacZnaky();
-
-    clearInterval(casovac);
-    casovac = setInterval(updateTime, 1000);
+  clearInterval(casovac);
+  casovac = setInterval(updateTime, 1000);
 }
 
 function oznacZnaky() {
-    rewriteText.innerText = "";
-    aktualnaHlaska[0].split('').forEach(znak => {
-        let span = document.createElement('span');
-        span.classList.add('span_char');
-        span.innerText = znak;
-        rewriteText.appendChild(span);
-    });
+  rewriteText.innerText = "";
+  aktualnaHlaska.content.split("").forEach((znak) => {
+    let span = document.createElement("span");
+    span.classList.add("span_char");
+    span.innerText = znak;
+    rewriteText.appendChild(span);
+  });
 }
 
 function updateTime() {
-    if (zostavajuciCas > 0) {
-        zostavajuciCas--;
-        uplynulyCas++;
-        casovacElement.innerText = `Zostáva: ${zostavajuciCas} s`;
-    } else {
-        ukonciHru();
-    }
+  if (zostavajuciCas > 0) {
+    zostavajuciCas--;
+    uplynulyCas++;
+    casovacElement.innerText = `Zostáva: ${zostavajuciCas} s`;
+  } else {
+    ukonciHru();
+  }
 }
 
-function resetTime() {
+function resetTime() {}
 
-}
-
-function ukonciHru() {
-
-}
+function ukonciHru() {}
 
 inizializuj();
